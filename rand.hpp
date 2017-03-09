b14096cd79bf8cca8332810356635e83a16bb746
fstqwq <fstqwq@foxmail.com>
2017-03-09 17:44:46 +0800

    Update rand.hpp

--- rand.hpp
+++ rand.hpp
@@ -1,12 +1,12 @@
 /* 2017-03-09, fstqwq.
 
-   This is a C++ head file in hope that it will help generating random numbers
-   using Xorshift-like algorithm, but without any warranty of course.
+   This is a C++ head file in hope that it will help generating Pseudorandom
+   numbers using Xorshift-like algorithm, but without any warranty of course.
 
-   This head file is shared under CC0 1.0 Universal license (CC0 1.0), which means
-   you can copy, modify, distribute and perform the work, even for commercial
-   purposes, all without asking permission. See other information at
-   <https://creativecommons.org/publicdomain/zero/1.0/>.  */
+   This head file is shared under CC0 1.0 Universal license (CC0 1.0), which
+   means you can copy, modify, distribute and perform the work, even for
+   commercial purposes, all without asking permission. See other information
+   at <https://creativecommons.org/publicdomain/zero/1.0/>.  */
 
 #ifndef _RAND_H
 
@@ -24,19 +24,20 @@
 #include <time.h>
 #endif
 
-unsigned long long __rand_value = 0x2333333333333333;
-
-void Srand(unsigned long long seed = 0x319) {
-	srand(int(time(NULL)));
-	__rand_value ^= (seed << 32) ^ seed ;
-	for (int i = 15; i; i--) __rand_value ^= (unsigned long long)rand() * RAND_MAX + rand(); 
-}
+unsigned long long __rand_value = 0x2333333333333333, __hidden_seed = 0x79786365766f6c69;
 
 unsigned long long raw_rand() {
 	__rand_value ^= (__rand_value - 1) >> 12;
 	__rand_value ^= (__rand_value + 1) << 25;
 	__rand_value ^= (__rand_value - 1) >> 27;
-	return __rand_value ^ 0x79786365766f6c69;
+	return __rand_value ^ __hidden_seed;
+}
+
+void Srand(unsigned long long seed = 0x319) {
+	srand(int(time(NULL) + seed));
+	__rand_value ^= time(NULL) * 0x2545f4914f6cdd1d;
+	__hidden_seed ^=  (seed << 32) ^ seed ^ raw_rand();
+	for (int i = 15; i; i--) __rand_value ^= (unsigned long long)rand() * RAND_MAX + rand(); 
 }
 
 int rand_int(int st = INT_MIN, int ed = INT_MAX) {
@@ -62,16 +63,15 @@ unsigned long long rand_ull(unsigned long long st = 0, unsigned long long ed = U
 }
 
 float rand_float(float st = 0, float ed = 1) {
-	return float(raw_rand() % ULLONG_MAX) / float(ULLONG_MAX - 1) * (ed - st) + st;
+	return float(raw_rand() % ULLONG_MAX)  * (ed - st) / float(ULLONG_MAX - 1) + st;
 }
 
 double rand_double(double st = 0, double ed = 1) {
-	return double(raw_rand() % ULLONG_MAX) / double(ULLONG_MAX - 1) * (ed - st) + st;
+	return double(raw_rand() % ULLONG_MAX) * (ed - st) / double(ULLONG_MAX - 1) + st;
 }
 
 long double rand_ldouble(long double st = 0, long double ed = 1) {
-	return ((long double)(raw_rand() % ULLONG_MAX)) / ((long double)ULLONG_MAX - 1) * (ed - st) + st;
+	return ((long double)(raw_rand() % ULLONG_MAX)) * (ed - st) / ((long double)ULLONG_MAX - 1) + st;
 }
 
 #endif
-
